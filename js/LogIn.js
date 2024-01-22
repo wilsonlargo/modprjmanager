6f8c080857ebdecbbbfc6a14e67e058557e5cf39
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-22 13:30:44 -0500

    a

--- js/LogIn.js
+++ js/LogIn.js
@@ -55,23 +55,7 @@ function maybeEnableButtons() {
      *  Sign in the user upon button click.
      */
 function handleAuthClick() {
-  tokenClient.callback = async (resp) => {
-    if (resp.error !== undefined) {
-      throw (resp);
-    }
-    document.getElementById('signout_button').style.visibility = 'visible';
-    document.getElementById('authorize_button').innerText = 'Actualizar';
-    await listarDatos();
-  };
-
-  if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
-    tokenClient.requestAccessToken({ prompt: 'consent' });
-  } else {
-    // Skip display of account chooser and consent dialog for an existing session.
-    tokenClient.requestAccessToken({ prompt: '' });
-  }
+  listarDatos()
 }
 
 /**
