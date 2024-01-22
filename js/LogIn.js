af4a1fd9d7ee3abc5146b1664581f1d8ee5b5791
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-22 15:22:36 -0500

    Update LogIn.js

--- js/LogIn.js
+++ js/LogIn.js
@@ -110,13 +110,11 @@ async function CrearFichas(datos) {
 }
 
 async function UpdateFica(){
-
-
-
-  responseDB = await gapi.client.sheets.spreadsheets.values.update({
+ let response
+  response = await gapi.client.sheets.spreadsheets.values.update({
     spreadsheetId: '16Lxewlh-ryS6y5d6BPA_HVAqYS4aCuZjq2IaD10mDkk',
     range: 'CONCEPT!A2:B2',
   });
-
+return response;
 }
 
