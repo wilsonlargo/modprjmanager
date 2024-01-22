e1f7765902ba5c8a77a2f9aa1c9636e41e43492b
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-21 20:58:31 -0500

    a

--- LogIn.js
+++ LogIn.js
@@ -1,17 +1,24 @@
-    // TODO(developer): Set to client ID and API key from the Developer Console
-    const CLIENT_ID = '965466288635-jen140lkpfuo74hvn1put856lbkhcg29.apps.googleusercontent.com';
-    const API_KEY = 'AIzaSyA7owurVh6Ev4GuK57d6n21NoEqeRcS8gg';
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = '965466288635-jen140lkpfuo74hvn1put856lbkhcg29.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyA7owurVh6Ev4GuK57d6n21NoEqeRcS8gg';
 
-    // Discovery doc URL for APIs used by the quickstart
-    const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
 
-    // Authorization scopes required by the API; multiple scopes can be
-    // included, separated by spaces.
-    const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
 
-    let tokenClient;
-    let gapiInited = false;
-    let gisInited = false;
+let tokenClient;
+let gapiInited = false;
+let gisInited = false;
 
-    document.getElementById('authorize_button').style.visibility = 'hidden';
-    document.getElementById('signout_button').style.visibility = 'hidden';
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
+
+/**
+* Callback after api.js is loaded.
+*/
+function gapiLoaded() {
+  gapi.load('client', initializeGapiClient);
+}
