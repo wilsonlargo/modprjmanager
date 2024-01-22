b7b089de0407a4c93b3201fcbc4062a95f4dd85b
wilsonlargo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-21 20:34:19 -0500

    a8

--- LogIn.js
+++ LogIn.js
@@ -18,6 +18,9 @@
     let gapiInited = false;
     let gisInited = false;
 
+    document.getElementById("gapi").addEventListener("load", gapiLoaded())
+    document.getElementById("gis").addEventListener("load", gisLoaded())
+
     document.getElementById('authorize_button').style.visibility = 'hidden';
     document.getElementById('signout_button').style.visibility = 'hidden';
 
