ab0606d3053aeb7671fac2c403d8c9b45fbc6958
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-23 15:32:54 +0000

    a

--- js/LogIn.js
+++ js/LogIn.js
@@ -9,6 +9,7 @@ const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
 // included, separated by spaces.
 const SCOPES = 'https://www.googleapis.com/auth/spreadsheets';
 
+
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
@@ -36,10 +37,25 @@ function gisLoaded() {
   gisInited = true;
 }
 
-
 function handleAuthClick() {
-  listarDatos();
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw (resp);
+    }
+    await getTurnos();
+    listarDatos();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+  }
 }
+
 
 
 async function listarDatos() {
