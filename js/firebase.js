fa65a9bbb9676ba2235fc6331316c32c09ac09ad
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-23 23:23:06 +0000

    a

--- js/firebase.js
+++ js/firebase.js
@@ -34,10 +34,12 @@ const objetivo = {
         }
     }
 }
-(async () => {   
+
+async function crear(){
     // Guardar un objeto e imprimir su id automatico despues de guardar
     const docRef = await addDoc(coleccionProyectos, objetivo);
     const key = docRef.id;
     console.log(key);
-})();
+}
+
 
