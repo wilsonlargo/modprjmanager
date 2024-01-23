30c3bf18864cdfe7ac5cb670519f3673fa77dcab
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-23 16:19:22 +0000

    a

--- js/LogIn.js
+++ js/LogIn.js
@@ -143,7 +143,7 @@ async function editTurno() {
 
   const filaAEditar = 2;
   response = await gapi.client.sheets.spreadsheets.values.update({
-    spreadsheetId: SPREADSHEET,
+    spreadsheetId: '16Lxewlh-ryS6y5d6BPA_HVAqYS4aCuZjq2IaD10mDkk',
     range: 'CONCEPT!A2:E2',
     //values: [update],
     //valueInputOption: "USER_ENTERED"
