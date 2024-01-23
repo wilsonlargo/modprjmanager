c455c060dae7579ea9599473438c29054ededc15
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-23 19:28:45 +0000

    a

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,5 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import {
     getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged,
     setPersistence, browserLocalPersistence, browserSessionPersistence
@@ -18,4 +19,38 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
+const coleccionProyectos = collection(db, "proyectos");
+const objetivo = {
+    nombre: "Nombre objetivo",
+    activas: {
+        "1": {
+            actividad: "nombre actvidad",
+            evidencias: [
+                "una",
+                "dos",
+            ]
+        }
+    }
+}
+
+(async () => {   
+    // Guardar un objeto e imprimir su id automatico despues de guardar
+    const docRef = await addDoc(coleccionProyectos, objetivo);
+    const key = docRef.id;
+    console.log(key);
+    
+    // Leer un documento en firebase en la coleccion proyectos con la id "key"
+    //const objRef = await getDoc(doc(db, "proyectos", key));
+    // Guarda los datos de la referencia del documento que cargamos y de paso le agrega la id
+    //const obj = {
+      //  id: objRef.id,
+        //...objRef.data(),
+    //}
+
+    // Lee todos los registros de una coleccion y lo guarda en un array con todos los objetos
+    //const refDatos = await getDocs(coleccionProyectos);
+    //const datos = refDatos.map(obj => obj.data());
+
+})();
