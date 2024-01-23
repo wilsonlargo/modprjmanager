405ff5ef3c60cd7c5d4d58c1256e5fecf802791c
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-23 23:55:39 +0000

    a

--- js/agregar.js
+++ js/agregar.js
@@ -12,9 +12,9 @@ async function crear() {
             }
         }
     }
-    
-          // Guardar un objeto e imprimir su id automatico despues de guardar
-          const docRef = await addDoc(coleccionProyectos, objetivo);
-          const key = docRef.id;
-          alert(key)
+
+    // Guardar un objeto e imprimir su id automatico despues de guardar
+    const docRef = await addDoc(coleccionProyectos, objetivo);
+    const key = docRef.id;
+    alert(key)
 }
