e34377a8a0f5ea5d5d54550bc6c9ea9a2030f6a5
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-24 00:27:43 +0000

    a

--- js/agregar.js
+++ js/agregar.js
@@ -1,4 +1,3 @@
-import { getFirestore, collection, doc, addDoc, setDoc, getDocs, getDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 async function crear() {
 
@@ -14,9 +13,9 @@ async function crear() {
             }
         }
     }
-
+    
     // Guardar un objeto e imprimir su id automatico despues de guardar
-    const docRef = await addDoc(coleccionProyectos, objetivo);
+    const docRef = await coleccionProyectos.addDoc(coleccionProyectos, objetivo);
     const key = docRef.id;
-    alert(key)
+    //alert(key)
 }
