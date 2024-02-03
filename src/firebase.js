167c9681dba82f291308ae6a96ebf43953b1af72
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-02-03 14:43:44 +0000

    add changes

---
+++ src/firebase.js
@@ -0,0 +1,93 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged,
+    setPersistence,
+    browserLocalPersistence,
+    browserSessionPersistence,
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    setDoc,
+    getDocs,
+    getDoc,
+    deleteDoc,
+    updateDoc,
+    deleteField,
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+// Utiliza las claves y credenciales de mi base de datos de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDpAJ6PAMfIe-fZ5TSh1NO_bi3Gnkk5LWI",
+    authDomain: "modtense.firebaseapp.com",
+    projectId: "modtense",
+    storageBucket: "modtense.appspot.com",
+    messagingSenderId: "780787597404",
+    appId: "1:780787597404:web:198f32a864505b22812699"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Referencia a las colecciones de proyectos y objetivos
+const coleccionProyectos = collection(db, "proyectos");
+
+/* Funciones base para manejar la base de datos de proyectos */
+
+// Función para obtener todos los proyectos de la base de datos
+async function getProyectos() {
+    const proyectos = [];
+    const querySnapshot = await getDocs(coleccionProyectos)
+    querySnapshot.forEach((doc) => {
+        proyectos.push({
+            ...doc.data(),
+            id: doc.id,
+        });
+    });
+    return proyectos;
+}
+
+// Función para agregar un objeto de proyecto a la base de datos
+async function addProyecto(objProyecto) {
+    const docRef = await addDoc(coleccionProyectos, objProyecto);
+    return docRef.id;
+}
+
+// Funcion para eliminar un proyecto por id
+async function borrarProyecto(id) {
+    await deleteDoc(doc(db, "proyectos", id));
+}
+
+// Función para obtener un proyecto por id
+async function getProyecto(id) {
+    const docRef = doc(db, "proyectos", id);
+    const docSnap = await getDoc(docRef);
+
+    return docSnap.exists() ? ({
+        ...docSnap.data(),
+        id: docSnap.id,
+    }) : null;
+}
+
+// Función para actualizar un proyecto
+async function updateProyecto(proyecto) {
+    const docRef = doc(db, "proyectos", proyecto.id);
+    await setDoc(docRef, proyecto);
+}
+
+// Exponer las funciones globalmente
+GLOBAL.firestore = {
+    getProyectos,
+    addProyecto,
+    borrarProyecto,
+    getProyecto,
+    updateProyecto,
+}
---
+++ src/util.js
@@ -0,0 +1,48 @@
+const GLOBAL = {
+    state: {
+        proyecto: null,
+    },
+    firestore: {},
+};
+
+function calcPorcenaje(cumplido, meta) {
+    return meta === 0 ? 0 : (cumplido / meta) * 100;
+}
+
+const CONSTANTS = {
+    indicadores: [
+        "DOCUMENTO",
+        "JORNADA",
+        "PERSONAS",
+        "UNIDAD",
+        "PESOS",
+        "ORGANIZACIÓN",
+        "REPORTE",
+        "INFORME",
+        "ACTA",
+        "PORCENTAJE",
+    ],
+    meses: {
+        ENERO: "Enero",
+        FEBRERO: "Febrero",
+        MARZO: "Marzo",
+        ABRIL: "Abril",
+        MAYO: "Mayo",
+        JUNIO: "Junio",
+        JULIO: "Julio",
+        AGOSTO: "Agosto",
+        SEPTIEMBRE: "Septiembre",
+        OCTUBRE: "Octubre",
+        NOVIEMBRE: "Noviembre",
+        DICIEMBRE: "Diciembre"
+    }
+}
+
+function addOptions(select, options) {
+    options.forEach(value => {
+        const option = document.createElement('option');
+        option.value = value;
+        option.textContent = value;
+        select.appendChild(option);
+    });
+}
